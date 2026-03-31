82399fd20599a6cc2e8e84785f7e314e5e843769
Asit Mandi <asitmandi86@gmail.com>
2026-03-31 09:46:14 +0530

    Update site

--- scritp.js
+++ scritp.js
@@ -3,7 +3,7 @@ const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
 menuOpenButton.addEventListener("click", () => {
     // Toggle mobile menu visibility
-    document.body.classList.toggle("s");
+    document.body.classList.toggle("show-mobile-menu");
 });
 // close menu when the close button is clicked
 menuCloseButton.addEventListener("click", () => menuOpenButton.click());
